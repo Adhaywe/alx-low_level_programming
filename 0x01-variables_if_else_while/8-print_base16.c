c98974196c06c3800ed0407dd080c3f9e7dd65cd
Adam <adamghebrezgi@gmail.com>
2022-03-11 20:04:45 +0300

    C program that print hexadecimal numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,13 @@ int main(void)
 	int ch;
 
 	for (num = 0; num < 10; num++)
+	{
 		putchar((num % 10) + '0');
-	fot (ch = 'a'; ch <= 'f'; ch++)
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
 		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
