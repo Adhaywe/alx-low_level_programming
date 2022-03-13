ff0a13c14c1215a5dab2b6f24faea15ea3698bf4
Adam <adamghebrezgi@gmail.com>
2022-03-13 20:31:53 +0300

    C program that prints a sentence exactly as stated in the question

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
 
 /**
  * main - Entry point
- * 
  * Return: ret 1 (Success)
  */
 int main(void)
 {
 	fprintf(
 	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	);
+);
 	return (1);
 }
