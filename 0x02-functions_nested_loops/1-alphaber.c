604b3f5daf11069ff8f74c20b7ec1aa1aa8192c9
Adam <adamghebrezgi@gmail.com>
2022-03-15 11:17:15 +0300

    A function that prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphaber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	int ch;
+	char lowcase;
+	
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		lowcase = putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
