00b154e3b777e4b2730d92f69427b5df7feec84e
Adam <adamghebrezgi@gmail.com>
2022-03-18 19:43:24 +0300

    Function that draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,12 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i <= n)
+		for (i = 1; i <= n; i++)
 		{
-			_putchar(95);
-			i++;
+			_putchar('_');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
