ef2b705b4a5d89f8ce3f130fbc00837d56ef809b
Adam <adamghebrezgi@gmail.com>
2022-03-21 17:36:21 +0300

    Function that prints string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @str: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, j;
+	char a[500];
+	i = 0;
+	j = 0;
+
+	while (*(s + i))
+	{
+		*(a + i) = *(s + i);
+		i++
+	}
+	i = i - 1;
+	while (i > = 0)
+	{
+		*(s + i) = *(a + j);
+		j++;
+		i--;
+	}
+}
