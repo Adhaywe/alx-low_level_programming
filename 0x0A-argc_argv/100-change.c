a70b3bac6136b8ced49a66060573ce80fffb4e1c
Adam <adamghebrezgi@gmail.com>
2022-04-01 16:41:04 +0300

    Program that prints its name

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc - number of command line arguments
+ * @argv - array of arguments
+ * Return: 0 if successful, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+
