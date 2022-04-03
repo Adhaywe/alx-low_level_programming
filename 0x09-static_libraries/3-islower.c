437b9e8a8eb32a15083a5f9807ec6b19c08afae0
Adam <adamghebrezgi@gmail.com>
2022-04-03 19:46:05 +0300

    Created a library containing various functions

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character
+ * @c: single character innput
+ * Return: 1 if lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
