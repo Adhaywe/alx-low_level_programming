5531d4131310c07fc358a9dddee16560c9ffd246
Adam <adamghebrezgi@gmail.com>
2022-04-11 09:56:08 +0300

    Program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
+#include "1-pi.h"
 #include <stdio.h>
 
 /**
- * main - program that prints the name of the file it was compiled from,
- * followed by a new line
+ * main - check the code for Holberton School students.
  *
- * Return - name of the file
+ * Return: Always 0.
  */
-int main()
+int main(void)
 {
-	printf("%s\n",_FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
