af34cea613fad7c57d797927b71b5008cabfe280
Adam <adamghebrezgi@gmail.com>
2022-04-14 12:18:15 +0300

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list c);
+void print_int(va_list i);
+void print_float(va_list f);
+void print_string(va_list s);
+
+/**
+ * struct format_t - struct type defining a format_t
+ * @t: symbol representing a data type
+ * @f: a function pointer to a function that prints a data type corresponding to 
+ * symbol
+ */
+typedef struct format_t
+{
+	char *t;
+	void (*f)(va_list);
+} format_t;
 
 #endif
