31b055d323062a9695b596a474ae7c5efdb8ce59
Adam <adamghebrezgi@gmail.com>
2022-04-14 12:50:59 +0300

    Function that prints 4 types of data types

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,10 +7,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list c);
-void print_int(va_list i);
-void print_float(va_list f);
-void print_string(va_list s);
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_string(va_list);
 
 /**
  * struct format_t - struct type defining a format_t
