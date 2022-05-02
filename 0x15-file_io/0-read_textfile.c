f94e1671f74b75fc6b4295cc2f79a3a25fd8029b
Adam <adamghebrezgi@gmail.com>
2022-05-02 15:11:39 +0300

    Reads a text file and prints it to the POSIX std output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, c, let;
+	ssize_t r, w, o;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters + 1);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL || filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (o == -1)
 		return (0);
 
-	let = read(fd, buf, letters);
-	if (let == -1)
-		return (0);
-	
-	buf[letters] = '\0';
-
-	c = write(1, buf, let);
-	if (c == -1)
+	r = read(o, buf, letters);
+	if (r == -1)
 		return (0);
 
-	close(fd);
+	w = write(STDOUT_FILENO, buf, r);
+
+	if (w == -1 || w != r)
+		return (0);
+
+	close(o);
 	free(buf);
-	return (c);
+	return (w);
 }
