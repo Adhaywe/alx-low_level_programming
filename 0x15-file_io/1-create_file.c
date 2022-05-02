f502c1f0d5c3e51100bfa2a3ae021efa38b15878
Adam <adamghebrezgi@gmail.com>
2022-05-02 14:50:09 +0300

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-	if (fd == NUL)
+	if (fd == NULL)
 		return (-1);
 
 	i = 0;
